24325c824679376b37573abfdfe014a4606e4cfb
PhNgocLinh <yuzulinh@gmail.com>
2024-10-28 16:04:55 +0700

    BT_2810

---
+++ src/main/java/Kaze/Repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package Kaze.Repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Kaze.Entity.CategoryEntity;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long>{
+	List<CategoryEntity> findByNameContaining (String name);
+	Page<CategoryEntity> findByNameContaining (String name, Pageable pageable);
+
+}
